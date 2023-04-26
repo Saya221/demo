2a9f5a1a6870c15243bb0372ed2046445514c2f6
Lê Hoàng Nguyên <le.hoang.nguyen.bedev@gmail.com>
2023-04-26 17:44:38 +0700

    [Changed] From mysql2 -> PG

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   # FE
-
-  root to: "static_pages#index"
-  get "/signup", to: "users#new"
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  resources :shared_urls, only: %i(new create)
-  resources :users, only: %i(create new show)
+  # root to: "static_pages#index"
+  # get "/signup", to: "users#new"
+  # get "/login", to: "sessions#new"
+  # post "/login", to: "sessions#create"
+  # delete "/logout", to: "sessions#destroy"
+  # resources :shared_urls, only: %i(new create)
+  # resources :users, only: %i(create new show)
 
   # BE
-
-  # root to: redirect("/swagger/index.html"
+  root to: redirect("/swagger/index.html")
   namespace :api, format: :json do
     namespace :v1 do
       resources :sign_up_users, only: %i(create)
@@ -31,6 +29,6 @@ Rails.application.routes.draw do
 
 
   # Custom Errors
-  match "*path", to: "application#render404", via: :all
-  match "*path", to: "application#render500", via: :all
+  # match "*path", to: "application#render404", via: :all
+  # match "*path", to: "application#render500", via: :all
 end
