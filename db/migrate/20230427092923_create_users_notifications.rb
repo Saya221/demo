fe9b382bae29925c6a219b3743d387c28c56fd4f
Lê Hoàng Nguyên <le.hoang.nguyen.bedev@gmail.com>
2023-05-05 17:43:45 +0700

    [Update] Partition Table

--- db/migrate/20230427092923_create_users_notifications.rb
+++ db/migrate/20230427092923_create_users_notifications.rb
@@ -10,14 +10,64 @@ class CreateUsersNotifications < ActiveRecord::Migration[7.0]
     end
 
     execute <<-SQL
+      CREATE OR REPLACE FUNCTION crc32(text_string text)
+      RETURNS bigint AS $$
+      DECLARE
+        tmp bigint;
+        i int;
+        j int;
+        byte_length int;
+        binary_string bytea;
+      BEGIN
+        IF text_string = '' THEN
+          RETURN 0;
+        END IF;
+
+        i = 0;
+        tmp = 4294967295;
+        byte_length = bit_length(text_string) / 8;
+        binary_string = decode(replace(text_string, E'\\\\', E'\\\\\\\\'), 'escape');
+        LOOP
+          tmp = (tmp # get_byte(binary_string, i))::bigint;
+          i = i + 1;
+          j = 0;
+          LOOP
+            tmp = ((tmp >> 1) # (3988292384 * (tmp & 1)))::bigint;
+            j = j + 1;
+            IF j >= 8 THEN
+                EXIT;
+            END IF;
+          END LOOP;
+          IF i >= byte_length THEN
+            EXIT;
+          END IF;
+        END LOOP;
+        RETURN (tmp # 4294967295);
+      END
+      $$ IMMUTABLE LANGUAGE plpgsql;
+    SQL
+
+    # Testing function crc32
+    # @conn = PG.connect(dbname: ENV["DATABASE_NAME"])
+    # def crc32(text_string)
+    #   result = nil
+    #   @conn.exec("SELECT crc32('#{text_string}') AS result") do |result_set|
+    #     result = result_set.getvalue(0,0).to_i
+    #   end
+    #   result
+    # end
+    # 10_000_000.times do
+    #   uuid = SecureRandom.uuid
+    #   print "." if Zlib.crc32(uuid) != crc32(uuid)
+    # end
+
+    execute <<-SQL
       CREATE OR REPLACE FUNCTION users_notifications_insert_trigger()
       RETURNS TRIGGER AS $$
       DECLARE
         partition_name TEXT;
-        partition_date DATE;
       BEGIN
-        partition_date := date_trunc('month', NEW.created_at)::date;
-        partition_name := 'users_notifications_' || TO_CHAR(partition_date, 'YYYY_MM');
+        partition_name := 'users_notifications_' || mod(crc32(NEW.id::text), 1000);
         IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname=partition_name) THEN
           EXECUTE 'CREATE TABLE ' || partition_name || ' (LIKE users_notifications INCLUDING CONSTRAINTS)';
         END IF;
