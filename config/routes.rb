da0cc7a90f386e995ad943d0c9aa5941ed9c4044
Danny <danny.nguyen.goldenowl@gmail.com>
2023-09-21 15:29:57 +0700

    [Fix] Turbo Hotwire config

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
   # FE
-  # root to: "static_pages#index"
-  # get "/signup", to: "users#new"
-  # get "/login", to: "sessions#new"
-  # post "/login", to: "sessions#create"
-  # delete "/logout", to: "sessions#destroy"
-  # resources :shared_urls, only: %i(new create)
-  # resources :users, only: %i(create new show)
+  extend FrontRoutes
 
   # BE
-  root to: redirect("/swagger/index.html")
-
   extend TurboHotwireRoutes
   extend ApiRoutes
   extend SidekiqRoutes
-
-  # Custom Errors
-  # match "*path", to: "application#render404", via: :all
-  # match "*path", to: "application#render500", via: :all
 end
